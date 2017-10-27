533847ee4a6769d27313d662fc0ee78810a3f8b5
Sara Rodriguez <sarah0neko@gmail.com>
2017-10-27 10:24:29 -0500

    Agregamos variable email

--- routes/activityCreate.js
+++ routes/activityCreate.js
@@ -116,7 +116,7 @@ function initCase(req,res) {
 function findCustIdByEmail(email, next) {
     console.log('findCustIdByEmail', email);
     var post_data = JSON.stringify({
-        "name":"findCustIdByEmail"
+        "name": email
     });
     var options = {
         'hostname':'kvader-developer-edition.na24.force.com'
@@ -223,7 +223,7 @@ function createCustomer(data, next) {
 function createCase(custId, email, priority, next) {
     console.log('createCase', custId);
     var post_data = JSON.stringify({
-        "name":"createCase"
+        "nameCreateCase": email
     });
     var options = {
         'hostname':'kvader-developer-edition.na24.force.com'
